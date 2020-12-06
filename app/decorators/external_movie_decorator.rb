d74383bd49d8b81071ed5dbbadc06e99d3b0226c
Dzmitry Aliakseichyk <dima_drakon@inbox.ru>
2020-12-06 16:35:42 +0100

    Made refactoring for Movies::ExternalData and ExternalMovieDecorator

--- app/decorators/external_movie_decorator.rb
+++ app/decorators/external_movie_decorator.rb
@@ -3,17 +3,17 @@
 class ExternalMovieDecorator
   REQUIRED_ATTRIBUTES = %w[rating plot poster].freeze
 
-  def initialize(movie)
-    @movie = movie
+  def initialize(movie_hash)
+    @movie_hash = movie_hash
   end
 
   def decorate
-    movie.slice(*REQUIRED_ATTRIBUTES).tap do |m|
+    movie_hash.slice(*REQUIRED_ATTRIBUTES).tap do |m|
       m['poster'] = "#{Movies::ExternalData::HOST}#{m['poster']}" if m['poster'].present?
     end
   end
 
   private
 
-  attr_reader :movie
+  attr_reader :movie_hash
 end
--- app/services/movies/external_data.rb
+++ app/services/movies/external_data.rb
@@ -6,19 +6,15 @@ module Movies
   class ExternalData
     HOST = 'https://pairguru-api.herokuapp.com'
     API_URL = '/api/v1/movies'
-    SUCCESS_HTTP_CODE = '200'
 
     def initialize(title)
       @title = title
     end
 
     def fetch
-      unless request.code == SUCCESS_HTTP_CODE
-        Rails.logger.error("[PAIRGURU API] Fetching data for movie #{title} is failed with message #{parsed_error_body}")
-        return {}
+      Rails.cache.fetch("external_movies_data/#{title}", expires_in: 12.hours) do
+        handle_response
       end
-
-      ExternalMovieDecorator.new(parsed_body).decorate
     end
 
     private
@@ -26,18 +22,25 @@ module Movies
     attr_reader :title
 
     def request
-      @request ||=
-        Rails.cache.fetch("external_movies_data/#{title}", expires_in: 12.hours) do
-          Net::HTTP.get_response(uri)
-        end
+      @request ||= Net::HTTP.get_response(uri)
     end
 
     def parsed_body
-      JSON.parse(request.body)['data']['attributes']
+      @parsed_body ||= JSON.parse(request.body)
     end
 
-    def parsed_error_body
-      JSON.parse(request.body)['message']
+    def attributes
+      parsed_body.dig('data', 'attributes')
+    end
+
+    def handle_response
+      case request
+      when Net::HTTPSuccess, Net::HTTPRedirection then
+        ExternalMovieDecorator.new(attributes).decorate
+      else
+        Rails.logger.warn(error_message)
+        {}
+      end
     end
 
     def uri
@@ -46,6 +49,10 @@ module Movies
 
     def encoded_title
       URI.encode(title)
+    end
+
+    def error_message
+      "[PAIRGURU API] Fetching data for movie #{title} is failed with message #{parsed_body['message']}"
     end
   end
 end
